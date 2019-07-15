2f5c98a767fbfeec78043250cc10cc8c250b1d9b
Zelin-Zeng <wernerzeng@gmail.com>
2019-07-14 18:41:36 -0700

    Init skeleton of project

---
+++ cli/src/main.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <snet.h>
+
+void println(const std::string &msg)
+{
+    std::cout << msg << std::endl;
+}
+
+void printHelp()
+{
+    std::stringstream buffer;
+    buffer << "SNetCli" << std::endl;
+    buffer << "-h\tPrint help" << std::endl;
+    buffer << "-v\tPrint current version" << std::endl;
+
+    println(buffer.str());
+}
+
+void printVersion()
+{
+    std::stringstream buffer;
+    buffer << "Current version: " << (unsigned)SNet::Version::majorVersion << "." << (unsigned)SNet::Version::minVersion;
+    println(buffer.str());
+}
+
+int main(int argc, char *argv[])
+{
+    std::vector<std::string> params;
+    for (int i = 0; i < argc; i++)
+    {
+        std::string argvStr = argv[i];
+        params.push_back(argvStr);
+    }
+
+    if (params.size() < 2)
+    {
+        println("Didn't receive any parameter.");
+        printHelp();
+        return 1;
+    }
+
+    if (params[1] == "-v")
+    {
+        printVersion();
+        return 0;
+    }
+
+    if (params[1] == "-h")
+    {
+        printHelp();
+        return 0;
+    }
+
+    return 0;
+}
---
+++ snet/snet.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+#include <iostream>
+
+namespace SNet {
+
+namespace Version
+{
+    inline const static uint8_t majorVersion = 0;
+    inline const static uint8_t minVersion = 1;
+} // namespace Version
+
+} // namespace SNet
